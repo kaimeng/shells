2593139821576080c3d5e11c6c55e6c7d6ea53a5
meng <mkk41112@ly.com>
2019-08-20 10:46:07 +0800

    xiugai

--- kubeadm_init.sh
+++ kubeadm_init.sh
@@ -2,16 +2,16 @@
 
 kubeadm init --config=kubernetes_config.yaml
 
+echo "kubeadm init successfully ..."
 
+# 使用集群
+rm -rf $HOME/.kube
 mkdir -p $HOME/.kube
 cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 chown $(id -u):$(id -g) $HOME/.kube/config
 
-kubectl get cs
-
+# 安装 pod network
 mkdir -p ~/k8s/
 cd ~/k8s
 curl -O https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
 kubectl apply -f kube-flannel.yml
-
-kubectl get pod –all-namespaces -o wide
