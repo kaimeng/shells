86cc5a2736ca858eebf86f4cce8e40536f6b66d4
meng <mkk41112@ly.com>
2019-09-23 18:52:51 +0800

    xiugai

--- proxy.sh
+++ proxy.sh
@@ -3,10 +3,7 @@
 proxyaddress=10.136.41.21:1087
 
 # 设置的代理
-masterip="10.136.21.110"
 echo "export http_proxy=http://$proxyaddress" >> ~/.bash_profile
 echo "export https_proxy=\$http_proxy" >> ~/.bash_profile
-ip=`hostname -I`
-ip2=${ip/ /,}
 echo "export no_proxy=10.96.0.0/12,10.244.0.0/16,10.136.21.0/8" >> ~/.bash_profile
 source ~/.bash_profile
